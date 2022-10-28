03d8075903f647178c58b1aae1a369c2691794f5
Bruno Krauthamer <brunokrauthamer@gmail.com>
2022-10-28 16:55:09 -0300

    req07

--- src/models/Category.js
+++ src/models/Category.js
@@ -1,11 +1,22 @@
 const CategorySchema = (sequelize, DataTypes) => {
   const CategoryTable = sequelize.define('Category', {
+    id: {
+      type: DataTypes.STRING,
+      primaryKey: true,
+    },
     name: DataTypes.STRING,
   },
   {
     tableName: 'category',
     underscored: true,
   });
+
+  CategoryTable.associate = (models) => {
+    CategoryTable.hasMany(models.Post_Category, {
+      foreignKey: 'category_id', as: 'posts_categories'
+    });
+  }
+
   return CategoryTable
 }
 
